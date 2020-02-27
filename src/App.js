18f9ca4a0c02f56586e92e55aad5a386779e5a59
Erick Trettel de Almeida <ericktrettel@gmail.com>
2020-02-27 11:27:46 -0300

    Basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form } from '@unform/web'
+import { Scope } from '@unform/core'
 import './App.css';
 
+import Input from './components/Form/input'
+
+const initialData = {
+  email: 'ericktrettel@gmail.com',
+  address: {
+    number: 10
+  }
+}
+
 function App() {
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+
+      <Form initialData={initialData} onSubmit={handleSubmit}>
+        <Input name="name" />
+        <Input type="email" name="email" />
+        <Input type="password" name="password" />
+        
+        <Scope path="address">
+          <Input name="street" />
+          <Input name="city" />
+          <Input type="number" name="number" />
+        </Scope>
+
+        <button type="submit">Enviar</button>
+      </Form>
     </div>
   );
 }
